97a48d531d4d64a053db57b4a089e064d8f35ae5
Shabonkerz <Shabonkerz@gmail.com>
2016-05-04 09:27:53 -0500

    add delay option to spellmangler

--- src/SpellMangler/index.js
+++ src/SpellMangler/index.js
@@ -1,8 +1,11 @@
 import SpellMangler from './base';
-import _ from 'lodash';
 import commander from 'commander';
 import readline from 'readline';
 import fs from 'fs';
+
+process.on('SIGINT', () => {
+	process.exit(0);
+});
 
 function readLines (path) {
 	return new Promise( (resolve, reject) => {
@@ -22,7 +25,8 @@ function readLines (path) {
 			});
 	});
 }
-let words, mangler;
+
+let words;
 
 function sleep (milliseconds) {
 	return new Promise((resolve, reject) => {
@@ -32,34 +36,46 @@ function sleep (milliseconds) {
 	});
 }
 
-commander
-	.version('0.0.1')
-	.usage('<path>')
-	.description('An unnnhalpppfol epp firrr curracteng yoour atricyous spellleng.')
-	.arguments('<path>')
-	.action(async function (path) {
 
-		try {
-			words = await readLines(path);
-			mangler = new SpellMangler();
 
-			for (let i = 0, length = words.length; i < length; i++) {
-				let value = null;
-				const gen = mangler.mangle(words[i]);
-				while ((value = gen.next().value) && value !== undefined)
-				{
-					process.stdout.write(value + '\n');
-					await sleep(200);
-				}
+
+async function init () {
+	let filePath;
+	commander
+		.version('0.0.1')
+		.usage('<path to words file>')
+		.description('An unnnhalpppfol epp firrr curracteng yoour atricyous spellleng.')
+		.arguments('<path>')
+		.option('-d, --delay <duration>', 'Interval at which to yield results')
+		.action((path) => {
+			try {
+				filePath = path;
+			} catch (e) {
+				process.stdout.write(e.message + '\n');
 			}
-		} catch (e) {
-			process.stdout.write(e.message + '\n');
+		})
+		.parse(process.argv);
+
+	// Error and abort when no path is given.
+	if (filePath === undefined) {
+		console.error('No path to words file given! Use --help or -h for usage instructions.');
+		process.exit(1);
+	}
+
+	// Attempt to read file and load the lines into the words array.
+	words = await readLines(filePath);
+
+	const mangler = new SpellMangler();
+
+	for (let i = 0, length = words.length; i < length; i++) {
+		let value = null;
+		const gen = mangler.mangle(words[i]);
+		while ((value = gen.next().value) && value !== undefined)
+		{
+			process.stdout.write(value + '\n');
+			await sleep(commander.delay || 0);
 		}
-	})
-	.parse(process.argv);
+	}
+};
 
-
-
-process.on('SIGINT', () => {
-	process.exit(0);
-});
+init();
